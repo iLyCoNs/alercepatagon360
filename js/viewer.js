046a00aae93330ebac5fe586bd27bb2b6f1a8643
iLyCoNs <perito.vidal@gmail.com>
2026-07-06 20:20:30 -0400

    Update viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -6199,7 +6199,7 @@ function bindPanoramaPointerEvents() {
         }
         const timeDiff = Date.now() - startTime; const moveDist = Math.sqrt( Math.pow(mock.clientX - startX, 2) + Math.pow(mock.clientY - startY, 2) );
         if (timeDiff < 500 && moveDist < 10) {
-            if (isArquitecto2Active && visor360) {
+            if (isArquitecto2Active && visor360 && !isDevModePinsActive) {
                 const isDbl = Date.now() - lastClickTime < 350;
                 arq2_onPanoramaClick(mock, isDbl);
                 lastClickTime = Date.now();
